ff9304883aa329a45f22cb716a801b44d4455a20
Nazif Bin Morshed <134962453+NajifVai69@users.noreply.github.com>
2024-03-13 06:14:34 +0000

    datatype of variable

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,14 @@
+"use strict";//treat js as newer version
+//alert(3+3) this only works in browser terminal not in node js
+let lname="Najif";
+let age=18;
+let isLogedin=false;
+//numbet 2 to power 53
+//bigint onek boro valuer jonne
+//string=""
+//boolean true/false
+//null datatype etar object
+//objet
+//symbol uniqueness find er jonne
+
+console.log(typeof lname)
