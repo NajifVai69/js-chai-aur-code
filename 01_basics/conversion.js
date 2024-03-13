a3391dbdbbce5b938ab8120ae77d27e1c725070c
Nazif Bin Morshed <134962453+NajifVai69@users.noreply.github.com>
2024-03-13 06:44:14 +0000

    conversion

---
+++ 01_basics/conversion.js
@@ -0,0 +1,9 @@
+let score="33abc"
+console.log(typeof score);
+value1=Number(score)
+console.log(value1);//abc re convert kora possible na ejonne NaN JODIO ETA KE number data type show kore
+console.log(typeof value1);//string reo number hishebe convert kore
+let newScore=null
+console.log(typeof newScore);
+value2=Number(newScore)
+console.log(value2);//null er number form 0 undefined er NaN(Not a Number) true er jonne 1 false 0
