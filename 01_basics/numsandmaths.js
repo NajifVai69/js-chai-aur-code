9501f8c638c155a7177bf66ed8f5ce65a5da4016
Nazif Bin Morshed <134962453+NajifVai69@users.noreply.github.com>
2024-03-14 15:10:56 +0000

    maths

---
+++ 01_basics/numsandmaths.js
@@ -0,0 +1,13 @@
+const score= 400
+console.log(score)//number
+const value= new Number(400)
+console.log(value)//object
+console.log(typeof value, typeof score)
+console.log(value.toString())//object to string
+console.log(value.toFixed(2))// decimal 2 ghor
+const num1=12.8942
+console.log(num1.toPrecision(3))//total 3 ghor 
+console.log(num1.toPrecision(2))// total 2 ghor
+console.log(Math.abs(-4))
+console.log(Math.round(4.6))
+console.log(Math.random())//always 0 theke 1
