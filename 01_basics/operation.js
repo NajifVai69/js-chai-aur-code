6b237cb0d0e5f38ffe2d0ed77d390f761ebf73d1
Nazif Bin Morshed <134962453+NajifVai69@users.noreply.github.com>
2024-03-14 06:47:29 +0000

    operations

---
+++ 01_basics/operation.js
@@ -0,0 +1,30 @@
+let s1="hello"
+let s2=" najif"
+console.log(s1+s2);
+let val1=1+'2'
+console.log(val1);
+//console.log(typeof val1);
+console.log('1'+2);
+console.log('1'+2+2);//122 first e string ejonne string er addition
+console.log(2+2+"1"+"1");// 411 first e int er addition
+
+
+let x = 3;
+const y = x++;
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+let a = 3;
+const b = ++a;
+console.log(`a:${a}, b:${b}`);//string r integer eksathe evabe print kore `${}`
+// Expected output: "a:4, b:4"
+
+
+
+let xa = 3;
+const ya = ++x;
+// xa is 4; ya is 4
+
+let x2 = 3n;
+const y2 = ++x2;
+// x2 is 4n; y2 is 4n
+
