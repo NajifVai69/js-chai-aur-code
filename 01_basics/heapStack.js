76121ce4cea0b06338e1907acca34ac29247e875
Nazif Bin Morshed <134962453+NajifVai69@users.noreply.github.com>
2024-03-14 13:56:41 +0000

    heap stack

---
+++ 01_basics/heapStack.js
@@ -0,0 +1,10 @@
+//heap e direct file store hoy r stack e filer reference
+//ejonne non primitive(heap) change korle pura file change hoye jay jeta stack e hoy na
+let user1={
+    mail: "@gmail",
+    varsity : "bracu"
+}
+let user2=user1
+user2.mail="@yahoo"
+console.log(user1)
+console.log(user2)//user 1 and 2 both er email change hoye jabe
