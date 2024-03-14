e484a8e9299cdec2df10d7b554ee507492111b16
Nazif Bin Morshed <134962453+NajifVai69@users.noreply.github.com>
2024-03-14 14:33:57 +0000

    string built in function

---
+++ 01_basics/string.js
@@ -0,0 +1,10 @@
+const name='Nazif'
+val1=69
+console.log(name+ " vai"+ 69)//not recommanded
+console.log(`Hello my name is ${name}`)
+const gameName=new String("fifa")
+console.log(gameName[0])
+console.log(typeof gameName)
+console.log(gameName.toUpperCase())//original value change hoy nai
+console.log(gameName.length)
+
