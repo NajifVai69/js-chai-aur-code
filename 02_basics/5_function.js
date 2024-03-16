d425e7eb60306da7a66daaecb506060ba8710bcb
Nazif Bin Morshed <134962453+NajifVai69@users.noreply.github.com>
2024-03-16 14:31:28 +0000

    little change

--- 02_basics/5_function.js
+++ 02_basics/5_function.js
@@ -2,3 +2,12 @@ function addition(num1,num2){
     return num1+num2
 }
 console.log(addition(5,8))
+function multiple_addition(...num1){
+    return num1
+}
+console.log(multiple_addition(6,7,8,9))
+function obj(anyobject){
+    return `hello ${anyobject.user}. Your product price is ${anyobject.price}`
+}
+const user={user:"Nazif",price:"18"}
+console.log(obj(user))
